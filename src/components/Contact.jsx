19f104165f9250fc83cc568fba4f9328cfc34a4a
Sandip <sandip.bagale34@gmail.com>
2023-11-30 21:56:22 +0545

    login signup contact converted to tailwind

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,43 +10,43 @@ return(
     <>
 
 
-    <div className='mainContactContainer'>
-    <div className='mainContactHeading'>
-    <h2 className='ContactHeadingFirst'>Contact Us</h2>
+    <div className='mainContactContainer h-[920px] w-screen bg-white'>
+    <div className='mainContactHeading border-b border-opacity-25 border-black mb-15 pb-15 absolute top-[10%] left-[10%]'>
+    <h2 className='ContactHeadingFirst mb-5'>Contact Us</h2>
     </div>
 
-   <div className='subContactContainer'>
+   <div className='subContactContainer flex flex-row absolute top-[20%] left-[10%]'>
     <div className='firstContactContainer'>
-        <h2 className='contactHeadingSecond'>Send your message</h2>
-        <form action="/contactus" method='post'>
+        <h2 className='contactHeadingSecond mb-7 text-9x1'>Send your message</h2>
+        <form className='flex flex-col absolute left-[5px] text-lg' action="/contactus" method='post'>
            
             <label htmlFor="name"> <b>Your Name</b></label>
-            <input type="text" name='name' id='name' placeholder=' Enter your name' required/>
+            <input className="mb-1 mt-2 w-96 h-10 rounded border-custom-gray border text-base" type="text" name='name' id='name' placeholder=' Enter your name' required/>
 
             <label htmlFor="email"><strong>Email Address</strong></label>
-            <input type="email" name='email' id='email' placeholder=' Enter your email address' required />
+            <input className='mb-1 mt-2 w-96 h-10 rounded border-custom-gray border text-base' type="email" name='email' id='email' placeholder=' Enter your email address' required />
            
         
             <label htmlFor="number"><strong>Mobile Number</strong></label>
-            <input type="text" name='number' id='number' placeholder=' +977 98xxxxxxxx'/>
+            <input className='mb-1 mt-2 w-96 h-10 rounded border-custom-gray border text-base' type="text" name='number' id='number' placeholder=' +977 98xxxxxxxx'/>
        
            
             
             <label htmlFor="message"><strong>Message</strong></label>
-            <textarea name="message" id="message" cols="30" rows="10"></textarea>
+            <textarea className='mt-1 mb-1 w-96 rounded-md border-gray-300' name="message" id="message" cols="30" rows="10"></textarea>
           
 
            <p>We want your input: questions, bug reports, complaints, praise, feature requests — every little bit helps.
              Let us know what we can do to improve TrekCompanion.</p>
 
-             <input type="submit" value="Send Message" className='sendMessage'/>
+             <input  type="submit" value="Send Message" className='sendMessage mb-1 mt-2 w-96 h-10 rounded border-custom-gray border text-base bg-red-600 text-white text-lg w-36'/>
 
         </form>
     </div>
         
-<div className='secondContactContainer'>
-<h2 className='contactHeadingThird'>Contact Details</h2>
-<ul className='contactUl '>
+<div className='secondContactContainer absolute left-[800px]'>
+<h2 className='contactHeadingThird pt-2'>Contact Details</h2>
+<ul className='contactUl list-none mt-16 text-lg font-normal grid gap-5'>
     <li>Butwal, Rupandehi, Nepal</li>
     <div classname='subContactUl'>
     <li><strong>Call: </strong>01-11111111</li>
@@ -63,3 +63,4 @@ return(
     
 )
 }
+
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,58 +1,113 @@
+// import React from 'react'
+// import { useNavigate } from 'react-router-dom'
+// import './signup.css'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import {faCircleXmark} from '@fortawesome/free-solid-svg-icons'
+// import { Link } from 'react-router-dom'
+// import axios from 'axios'
+
+// export default function Signup() {
+//     const navigate = useNavigate()//location haru lai navigate garda chainxa
+
+//     const createUser = async (e)=>{
+//        e.preventDefault() //page refresh huna bata bachako natra data lost huna sakxa
+      
+//       //FormData le tyo form ko input field ko name use garera data linxa
+//       const formData = new FormData(e.currentTarget) //FormData le form ko data have ARRAY ko form ma linxa 
+//       const data = Object.fromEntries(formData) //object.fromEntries le tyo ARRAY lai OBJECT(KEY:VALUE) ma change garxa
+  
+//   //axios.post le arko argument object ma linxa tesaile convert gareko mathiko data lai object ma
+   
+//       //send above states data to api
+//      const response = await axios.post("http://localhost:2000/register", data) 
+//      if (response.status == 201){
+//       alert(response.data.message)
+//       navigate("/")
+//     }else{
+//       alert("Something went wrong")
+//      }
+//   }
+// return(
+//   <>
+//   <div className="signupPage">
+//   <div className="signupContainer">
+//         <h2>Registration Form</h2>
+//         <form onSubmit={createUser} method='post'>
+//             <div className="Form-group">
+//                 <label htmlFor="email">Email:</label>
+//                 <input type="email" id="email" name="email" required/>
+//             </div>
+//             <div className="Form-group">
+//                 <label htmlFor="username">Username:</label>
+//                 <input type="text" id="username" name="username" required/>
+//             </div>
+//             <div className="Form-group">
+//                 <label htmlFor="password">Password:</label>
+//                 <input type="password" id="password" name="password" required/>
+//             </div>
+//             <div className="Form-group">
+//                 <input type="submit" value="register"/>
+//             </div>
+//         </form>
+//             <Link  to="/loginForm">Already have an account?</Link>    
+//     </div>
+//     </div> 
+// </>
+// )
+
+// }
+
+
+
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import './signup.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCircleXmark} from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 export default function Signup() {
-    const navigate = useNavigate()//location haru lai navigate garda chainxa
+   const navigate = useNavigate()
 
-    const createUser = async (e)=>{
-       e.preventDefault() //page refresh huna bata bachako natra data lost huna sakxa
-      
-      //FormData le tyo form ko input field ko name use garera data linxa
-      const formData = new FormData(e.currentTarget) //FormData le form ko data have ARRAY ko form ma linxa 
-      const data = Object.fromEntries(formData) //object.fromEntries le tyo ARRAY lai OBJECT(KEY:VALUE) ma change garxa
-  
-  //axios.post le arko argument object ma linxa tesaile convert gareko mathiko data lai object ma
-   
-      //send above states data to api
-     const response = await axios.post("http://localhost:2000/register", data) 
-     if (response.status == 201){
-      alert(response.data.message)
-      navigate("/")
-    }else{
-      alert("Something went wrong")
-     }
-  }
-return(
-  <>
-  <div className="signupPage">
-  <div className="signupContainer">
-        <h2>Registration Form</h2>
-        <form onSubmit={createUser} method='post'>
-            <div className="Form-group">
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required/>
-            </div>
-            <div className="Form-group">
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" required/>
-            </div>
-            <div className="Form-group">
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required/>
-            </div>
-            <div className="Form-group">
-                <input type="submit" value="register"/>
-            </div>
-        </form>
-            <Link  to="/loginForm">Already have an account?</Link>    
-    </div>
-    </div> 
-</>
-)
+   const createUser = async (e)=>{
+      e.preventDefault()
+      const formData = new FormData(e.currentTarget)
+      const data = Object.fromEntries(formData)
+      const response = await axios.post("http://localhost:2000/register", data) 
+      if (response.status == 201){
+       alert(response.data.message)
+       navigate("/")
+     }else{
+       alert("Something went wrong")
+      }
+   }
 
+   return(
+     <>
+     <div className="w-screen h-screen">
+     <div className="max-w-[400px] mx-auto p-5 bg-white shadow-md rounded-md mt-5">
+           <h2 className="text-center mb-5">Registration Form</h2>
+           <form onSubmit={createUser} method='post'>
+               <div className="mb-5">
+                  <label className="block text-black font-bold">Email:</label>
+                  <input type="email" id="email" name="email" required className="w-full p-2 border border-gray-300 rounded-md"/>
+               </div>
+               <div className="mb-5">
+                  <label className="block text-black font-bold">Username:</label>
+                  <input type="text" id="username" name="username" required className="w-full p-2 border border-gray-300 rounded-md"/>
+               </div>
+               <div className="mb-5">
+                  <label className="block text-black font-bold">Password:</label>
+                  <input type="password" id="password" name="password" required className="w-full p-2 border border-gray-300 rounded-md"/>
+               </div>
+               <div className="mb-5">
+                  <input type="submit" value="register" className="w-full p-2 bg-blue-500 text-white cursor-pointer hover:bg-blue-700"/>
+               </div>
+           </form>
+           <Link to="/loginForm">Already have an account?</Link>   
+       </div>
+       </div> 
+   </>
+   )
 }
+
